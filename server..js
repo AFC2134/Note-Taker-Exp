cf6f859c702e3ccb5e24841d59386bb33ec60695
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-10-26 00:21:53 -0500

    app.use express fuction

--- server..js
+++ server..js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
 });
